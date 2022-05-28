2274e0c54e7bd7c5dbf96aa75165f86ef1d7c584
haltim <timoayomide2000@gmail.com>
2022-05-28 17:20:53 +0100

    Updates

---
+++ precache-manifest.31ace48f398354ac298943558fefb354.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ba1c18487f55271f7a3a",
+    "url": "/reactreduxshoppingpage/static/css/main.1438bb89.chunk.css"
+  },
+  {
+    "revision": "ba1c18487f55271f7a3a",
+    "url": "/reactreduxshoppingpage/static/js/main.ba1c1848.chunk.js"
+  },
+  {
+    "revision": "94d5cda26fd44b83dfcf",
+    "url": "/reactreduxshoppingpage/static/css/1.b0de0591.chunk.css"
+  },
+  {
+    "revision": "94d5cda26fd44b83dfcf",
+    "url": "/reactreduxshoppingpage/static/js/1.94d5cda2.chunk.js"
+  },
+  {
+    "revision": "1ed9f2e921c183b02722",
+    "url": "/reactreduxshoppingpage/static/js/runtime~main.1ed9f2e9.js"
+  },
+  {
+    "revision": "dd062e4ee4e6a67706770e3b3b10b308",
+    "url": "/reactreduxshoppingpage/index.html"
+  }
+];
